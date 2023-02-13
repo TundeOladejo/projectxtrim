29a2ab84dfc21e60243efa3296ddb685bee5bd13
Babatunde Oladejo <tundeoladejo@outlook.com>
2023-02-13 01:47:17 +0100

    resolve SharedArrayBuffer issue

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-// import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,7 +7,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-    // crossOriginIsolation()
   ],
   resolve: {
     alias: {
